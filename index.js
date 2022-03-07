81e3b2e01b9d3e680db84cc63983a650901cccac
+ <+>
2022-03-07 13:30:10 -0800

    commit tree e245fe963ab407c502581fea480be0049b854f49

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (x = 2000, y = 100) => {
+    return x / y 
+}
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+    add(3,3); 
+
+
+const square = (parameter) => parameter * parameter;
+square(4) ; 
+
+
+
+
+
+
